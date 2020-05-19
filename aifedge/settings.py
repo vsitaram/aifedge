237de821e4d22b834d1d0a68ad6b39f7f021101f
Ram Vuppaladadiyam <vsitaram@gmail.com>
2020-05-19 13:56:05 -0600

    Refactor backend and JS code and switch to AJAX in dashboard

--- aifedge/settings.py
+++ aifedge/settings.py
@@ -65,10 +65,10 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
 
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:8000',
-#     'https://onefigrapp.herokuapp.com'
-# )
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+    'https://onefigrapp.herokuapp.com'
+)
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_PERMISSION_CLASSES': [
