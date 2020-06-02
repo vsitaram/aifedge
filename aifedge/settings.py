05c10a6ae2d2b06b081bd2c3dd8bce053f1dbb96
Ram Vuppaladadiyam <vsitaram@gmail.com>
2020-06-01 21:08:22 -0600

    Change links and local settings

--- aifedge/settings.py
+++ aifedge/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'aifedge.herokuapp.com', 'aifedge.com']
-ALLOWED_HOSTS = ['127.0.0.1', 'aifedge.herokuapp.com']
+ALLOWED_HOSTS = ['aifedge.herokuapp.com']
 
 
 # Application definition
